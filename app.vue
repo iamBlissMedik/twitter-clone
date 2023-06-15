32e5664f5700a136df3766392489c784c31a675b
Ellams Bliss <ellamsbliss@gmail.com>
2023-06-16 01:06:25 +1000

    sidebar left done

--- app.vue
+++ app.vue
@@ -4,6 +4,24 @@ const darkMode = ref(false);
 
 <template>
   <div :class="{ dark: darkMode }">
-    <h1 class="text-red-500 dark:text-dim-500">Twitter clone</h1>
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- left side bar -->
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SideBarLeft />
+            </div>
+          </div>
+          <!--middle/main content  -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+            <h1>dhs</h1>
+          </main>
+          <!-- right side bar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
---
+++ components/SideBar/Left/index.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { HomeIcon } from "@heroicons/vue/24/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+  EllipsisHorizontalCircleIcon,
+} from "@heroicons/vue/24/outline";
+const { defaultTransition } = useTailwindConfig();
+</script>
+
+<template>
+  <div class="h-screen flex flex-col">
+    <div
+      class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+    <div class="mt-2 space-y-3">
+      <SideBarLeftTab active>
+        <template #icon>
+          <HomeIcon />
+        </template>
+        <template #name> Home </template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <BellIcon />
+        </template>
+        <template #name> Notifications </template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <HashtagIcon />
+        </template>
+        <template #name> Explore </template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <InboxIcon />
+        </template>
+        <template #name> Messages </template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <BookmarkIcon />
+        </template>
+        <template #name> Bookmarks </template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <DocumentTextIcon />
+        </template>
+        <template #name> Lists</template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <UserIcon />
+        </template>
+        <template #name> Profile </template>
+      </SideBarLeftTab>
+      <SideBarLeftTab>
+        <template #icon>
+          <EllipsisHorizontalCircleIcon />
+        </template>
+        <template #name> More </template>
+      </SideBarLeftTab>
+    </div>
+  </div>
+</template>
