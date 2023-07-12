be201853d472ce9d99a758b589085eb34f5302be
Ellams Bliss <ellamsbliss@gmail.com>
2023-07-13 01:26:13 +1000

    tweets from the be

--- components/Tweet/ListFeed.vue
+++ components/Tweet/ListFeed.vue
@@ -1,6 +1,19 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  tweets: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
 <template>
   <div>
     <h1>List feed</h1>
+
+    <ul>
+      <li v-for="tweet in tweets">
+        {{ tweet }}
+      </li>
+    </ul>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <script setup>
 const { twitterBorderColor } = useTailwindConfig();
-const loading = ref(false);
 const { useAuthUser } = useAuth();
+const { getHomeTweets } = useTweets();
 const user = useAuthUser();
+const loading = ref(false);
+const homeTweets = ref([]);
+onBeforeMount(async () => {
+  loading.value = true;
+  try {
+    const { tweets } = await getHomeTweets();
+    homeTweets.value = tweets;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
 <template>
   <div>
@@ -13,7 +26,7 @@ const user = useAuthUser();
       <div class="border-b" :class="twitterBorderColor">
         <TweetForm :user="user" />
       </div>
-      <TweetListFeed />
+      <TweetListFeed :tweets="homeTweets" />
     </MainSection>
   </div>
 </template>
