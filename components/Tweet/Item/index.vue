c20ca2bdc581e1dffa91e6f19d88fcf7b614fc8f
Ellams Bliss <ellamsbliss@gmail.com>
2023-07-13 17:36:12 +1000

    tweet feed

---
+++ components/Tweet/Item/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  tweet: {
+    type: Object,
+    required: true,
+  },
+});
+const { twitterBorderColor } = useTailwindConfig();
+</script>
+<template>
+  <div>
+    <TweetItemHeader :tweet="tweet" />
+    <div class="ml-16">
+      <p class="flex-shrink w-auto font-medium text-gray-800">
+        {{ tweet.text }}
+      </p>
+      <div
+        v-for="image in tweet.mediaFiles"
+        :key="image.id"
+        class="flex my-3 mr-2 border-2 rounded-2xl"
+        :class="twitterBorderColor"
+      >
+        <img :src="image.url" class="w-full rounded-2xl" />
+      </div>
+      <div class="mt-2">
+        action buttons
+      </div>
+    </div>
+  </div>
+</template>
--- components/Tweet/ListFeed.vue
+++ components/Tweet/ListFeed.vue
@@ -1,19 +1,26 @@
 <script setup>
+const { twitterBorderColor, defaultTransition } = useTailwindConfig();
 const props = defineProps({
   tweets: {
     type: Array,
     required: true,
   },
 });
+const isEmptyArray = computed(() => props.tweets.length === 0);
 </script>
 <template>
   <div>
-    <h1>List feed</h1>
-
-    <ul>
-      <li v-for="tweet in tweets">
-        {{ tweet }}
-      </li>
-    </ul>
+    <div v-if="isEmptyArray" class="p-4">
+      <p class="text-center text-gray-500">No tweets 🥲</p>
+    </div>
+    <div
+      v-else
+      class="pb-4 border-b cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300"
+      :class="[twitterBorderColor, defaultTransition]"
+      v-for="tweet in tweets"
+      :key="tweet.id"
+    >
+      <TweetItem :tweet="tweet" />
+    </div>
   </div>
 </template>
